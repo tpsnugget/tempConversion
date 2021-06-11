1e85762cbd23b74fe708fde80fe9f92cc523a0ce
Mike Giebner <mikegiebner@gmail.com>
2021-06-10 20:56:16 -0400

    Added basic layout for <AddGrade /> Component

--- client/src/components/AddGrade.js
+++ client/src/components/AddGrade.js
@@ -1,17 +1,54 @@
 import React, { useEffect, useState } from 'react'
-// import axios from 'axios'
-// import Box from '@material-ui/core/Box'
-// import Container from '@material-ui/core/container'
+import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
-// import { node_server } from '../helpers'
-// import PropTypes from 'prop-types'
-// import TextField from '@material-ui/core/TextField'
-// import Typography from '@material-ui/core/Typography'
+import PropTypes from 'prop-types'
+import TextField from '@material-ui/core/TextField'
 
 const AddGrade = ({}) => {
+
+   const [ student, setStudent ] = useState('')
+   const [ teacher, setTeacher ] = useState('')
+
    return (
-      <Grid>
-         Add a Grade
+      <Grid container style={{marginTop: '2rem'}}>
+
+         <Grid container>
+            <Grid item xs={3}>
+               <TextField label='Add a Teacher (last, first)' variant='outlined'  style={{width: '100%'}} value={teacher} />
+            </Grid>
+            <Grid item xs={3}>
+               <TextField label='Add a Student (last, first)' variant='outlined'  style={{width: '100%', marginLeft: '0.5rem'}} value={student} />
+            </Grid>
+         </Grid>
+
+         <Grid container style={{marginTop: '1rem'}}>
+            <Grid item xs={3}>
+               <TextField label='Add a Teacher (last, first)' variant='outlined'  style={{width: '100%'}} value={teacher} />
+            </Grid>
+            <Grid item xs={3}>
+               <TextField label='Add a Student (last, first)' variant='outlined'  style={{width: '100%', marginLeft: '0.5rem'}} value={student} />
+            </Grid>
+            <Grid item xs={3}>
+               <TextField label='Add a Student (last, first)' variant='outlined'  style={{width: '100%', marginLeft: '1rem'}} value={student} />
+            </Grid>
+            <Grid item xs={1}>
+               {false ?
+                  <Button color='primary' variant='contained'  style={{height: '100%', marginLeft: '1.5rem'}} >Submit</Button>
+                  :
+                  <Button color='primary' variant='contained' disabled style={{height: '100%', marginLeft: '1.5rem'}} >Submit</Button>
+               }
+            </Grid>
+         </Grid>
+
+         <Grid container style={{marginTop: '1rem'}}>
+            <Grid item xs={3}>
+               <TextField label='Add a Teacher (last, first)' variant='outlined'  style={{width: '100%'}} value={teacher} />
+            </Grid>
+            <Grid item xs={3}>
+               <TextField label='Add a Student (last, first)' variant='outlined'  style={{width: '100%', marginLeft: '0.5rem'}} value={student} />
+            </Grid>
+         </Grid>
+
       </Grid>
    )
 }
